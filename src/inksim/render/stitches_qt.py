722efdc689e62ee5a813689999d174225af1b596
karnigen <karnigen@gmail.com>
2026-08-12 08:38:36 +0200

    Batch Qt stitches without changing draw order

--- src/inksim/render/stitches_qt.py
+++ src/inksim/render/stitches_qt.py
@@ -1,5 +1,6 @@
 """Qt-backed stitch rendering helpers."""
 
+import numpy as np
 from PySide6.QtCore import QLineF, Qt
 from PySide6.QtGui import QColor, QPainter, QPen
 
@@ -21,14 +22,20 @@ def render_simple_qt(
     pen.setWidthF(max(1.0, line_width * zoom))
     pen.setCapStyle(Qt.RoundCap)
     pen.setJoinStyle(Qt.RoundJoin)
-    for stitch in stitches[:visible_count]:
-        pen.setColor(QColor(int(stitch[4]), int(stitch[5]), int(stitch[6])))
+    visible_stitches = stitches[:visible_count]
+    endpoints = visible_stitches[:, :4].copy()
+    endpoints[:, 0::2] = endpoints[:, 0::2] * zoom + pan_x
+    endpoints[:, 1::2] = endpoints[:, 1::2] * zoom + pan_y
+    colors = visible_stitches[:, 4:7].astype(np.uint8)
+    color_changes = np.any(colors[1:] != colors[:-1], axis=1)
+    run_starts = np.concatenate(([0], np.flatnonzero(color_changes) + 1))
+    run_ends = np.concatenate((run_starts[1:], [len(colors)]))
+
+    for start, end in zip(run_starts, run_ends):
+        color = colors[start]
+        pen.setColor(QColor(int(color[0]), int(color[1]), int(color[2])))
         painter.setPen(pen)
-        painter.drawLine(
-            QLineF(
-                stitch[0] * zoom + pan_x,
-                stitch[1] * zoom + pan_y,
-                stitch[2] * zoom + pan_x,
-                stitch[3] * zoom + pan_y,
-            )
+        group = endpoints[start:end]
+        painter.drawLines(
+            [QLineF(x1, y1, x2, y2) for x1, y1, x2, y2 in group]
         )
