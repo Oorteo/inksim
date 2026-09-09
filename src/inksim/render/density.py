64e2e3f6b613f704f7029061b76f0dfb184cf842
karnigen <karnigen@gmail.com>
2026-09-09 19:57:03 +0200

    Add future imports to numba render modules

--- src/inksim/render/density.py
+++ src/inksim/render/density.py
@@ -1,6 +1,10 @@
 # SPDX-FileCopyrightText: 2026 Authors (see git history)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+"""Stitch density rendering helpers."""
+
+from __future__ import annotations
+
 import numba
 import numpy as np
 
